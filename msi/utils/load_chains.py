d5331d011e1edfb76e22c351bbf3c2a146982063
Arne Thomsen <athomsen@phys.ethz.ch>
2023-06-30 15:46:28 +0200

    refactor of the inference methods

---
+++ msi/utils/load_chains.py
@@ -0,0 +1,42 @@
+"""
+Created June 2023
+Author: Arne Thomsen
+
+Utils to load MCMC chains to be plotted.
+"""
+
+import os, h5py
+
+from msfm.utils import logger
+
+LOGGER = logger.get_logger(__file__)
+
+
+def load_preds(base_dir, model_dir, n_steps=None, file_label=None):
+    out_dir = os.path.join(base_dir, model_dir)
+
+    if n_steps is None:
+        preds_file = os.path.join(out_dir, f"preds.h5")
+    elif file_label is None:
+        preds_file = os.path.join(out_dir, f"preds_{n_steps}.h5")
+    else:
+        preds_file = os.path.join(out_dir, f"preds_{n_steps}_{file_label}.h5")
+
+    with h5py.File(preds_file, "r") as f:
+        # fiducial
+        fidu_train_preds = f["fiducial/train/pred"][:]
+        fidu_vali_preds = f["fiducial/vali/pred"][:]
+
+        LOGGER.info(f"fidu_train_preds =   {fidu_train_preds.shape}")
+        LOGGER.info(f"fidu_vali_preds =    {fidu_vali_preds.shape}")
+
+        # grid
+        grid_preds = f["grid/pred"][:]
+        grid_cosmos = f["grid/cosmo"][:]
+        grid_sobol = f["grid/i_sobol"][:]
+
+        LOGGER.info(f"grid_preds.shape =   {grid_preds.shape}")
+        LOGGER.info(f"grid_cosmos.shape =  {grid_cosmos.shape}")
+        LOGGER.info(f"grid_sobol.shape =   {grid_cosmos.shape}")
+
+    return fidu_train_preds, fidu_vali_preds, grid_preds, grid_cosmos, grid_sobol
---
+++ msi/utils/plotting.py
@@ -0,0 +1,88 @@
+"""
+Created June 2023
+Author: Arne Thomsen
+
+Utils to plot the 1D and 2D marginals of samples from a posterior distribution.
+"""
+
+import os
+import numpy as np
+
+from trianglechain import TriangleChain
+from seaborn import color_palette
+
+from msfm.utils import parameters, logger
+
+LOGGER = logger.get_logger(__file__)
+
+method_label_dict = {
+    "gp_abc": "GP ABC",
+    "flow_likelihood": "flow (likelihood)",
+    "flow_posterior": "flow (posterior)",
+    "gaussian_mixture": "Gaussian (mixture)",
+    "gaussian_likelihood": "Gaussian (likelihood)",
+}
+
+param_label_dict = {
+    "Om": r"$\Omega_m$",
+    "s8": r"$\sigma_8$",
+    "Aia": r"$A_{IA}$",
+    "n_Aia": r"$\eta_{A_{IA}}$",
+    "bg": r"$b_g$",
+    "n_bg": r"$\eta_{b_g}$",
+}
+
+
+def make_contour_plot(
+    model_dir,
+    n_steps,
+    params,
+    out_dir=None,
+    out_file="contours.png",
+    methods=["gp_abc", "flow_likelihood", "flow_posterior", "gaussian_mixture", "gaussian_likelihood"],
+    scale_to_prior=True,
+):
+    assert all(
+        method in method_label_dict.keys() for method in methods
+    ), f"Only {method_label_dict.keys()} are allowed"
+
+    # cosmetics
+    color_iter = iter(color_palette("tab10"))
+
+    if scale_to_prior:
+        ranges = dict(zip(params, parameters.get_prior_intervals(params)))
+    else:
+        ranges = None
+
+    # initialize plot
+    tri = TriangleChain(
+        labels=[param_label_dict[param] for param in params],
+        scatter_kwargs={"s": 500, "marker": "*", "zorder": 299},
+        grid=True,
+        fill=True,
+        show_values=False,
+        bestfit_method="median",
+        ranges=ranges,
+    )
+
+    # plot contours
+    for method in methods:
+        chain = np.load(os.path.join(model_dir, f"chain_{n_steps}_{method}.npy"))
+        tri.contour_cl(chain, names=params, label=method_label_dict[method], color=next(color_iter))
+
+    # plot fiducial
+    tri.scatter(
+        dict(zip(params, parameters.get_fiducials(params))),
+        label="fiducial",
+        plot_histograms_1D=False,
+        color="k",
+        show_legend=True,
+        scatter_vline_1D=True,
+    )
+
+    # save figure
+    if out_dir is not None:
+        os.makedirs(out_dir, exist_ok=True)
+        tri.fig.savefig(os.path.join(out_dir, out_file), bbox_inches="tight", dpi=300)
+    else:
+        LOGGER.warning(f"Not saving the plot")
